a8c6945f5cbf2fe0b208d9ca541ea3177a4fe878
李赓 <ligeng@1dabang.cn>
2017-01-17 19:08:43 +0800

    update

--- src/main/java/com/ydb/controller/TestController.java
+++ src/main/java/com/ydb/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.ydb.controller;
 
+import com.ydb.entity.nochange.Issue;
+import com.ydb.mapper.nochange.IssueMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * Created by ligeng on 17/1/15.
@@ -10,8 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 public class TestController {
+    @Autowired
+    private IssueMapper issueMapper;
+
     @GetMapping("index")
     public String index(){
+        Issue issue = new Issue();
+        issue.setUID(UUID.randomUUID().toString());
+        issueMapper.insert(issue);
         return "这是测试主页";
     }
 }
